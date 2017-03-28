c6136f073def0861c181ab2edcbc25d2923ed451
Gabriel Díaz López de la Llave <g.diaz.loepzllave@bbva.com>
2017-03-29 00:13:03 +0200

    remove wilcard from hadoop perms

--- hdfs/boot.sh
+++ hdfs/boot.sh
@@ -175,8 +175,8 @@ trap "shut_down sigint" SIGINT
 core_site_default=(
 	"fs.defaultFS=hdfs://${cluster_name}:8020"
 	"io.file.buffer.size=131072"
-	"hadoop.proxyuser.openshift.host=*"
-	"hadoop.proxyuser.openshift.groups=*"
+	"hadoop.proxyuser.openshift.host=172.16.0.0/20"
+	"hadoop.proxyuser.openshift.groups=openshift,root"
 )
 
 # https://log.rowanto.com/why-datanode-is-denied-communication-with-namenode/
