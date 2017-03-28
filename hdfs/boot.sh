6e43773d7725ea60d9f6c08b9554ea192e596fe5
Gabriel Díaz López de la Llave <g.diaz.loepzllave@bbva.com>
2017-03-28 22:43:07 +0200

    httpfs extra config

--- hdfs/boot.sh
+++ hdfs/boot.sh
@@ -169,9 +169,13 @@ trap "shut_down sigint" SIGINT
 
 
 # default config
+# For httpfs config
+# https://hadoop.apache.org/docs/current/hadoop-project-dist/hadoop-common/Superusers.html
 core_site_default=(
 	"fs.defaultFS=hdfs://${cluster_name}:8020"
 	"io.file.buffer.size=131072"
+	"hadoop.proxyuser.openshift.host=hdfs-httpfs"
+	"hadoop.proxyuser.openshift.groups=*"
 )
 
 # https://log.rowanto.com/why-datanode-is-denied-communication-with-namenode/
