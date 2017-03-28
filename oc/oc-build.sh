ae077deb06f73fa03d12dd91d613c2430da19abf
Gabriel Díaz López de la Llave <g.diaz.loepzllave@bbva.com>
2017-03-28 18:36:30 +0200

    fix oc to focus on minishift deployment

--- oc/oc-build.sh
+++ oc/oc-build.sh
@@ -6,12 +6,12 @@ export repository="ssh://git@globaldevtools.bbva.com:7999/bglh/docker-hdfs-allux
 export secretname="sshcert"
 
 # https://docs.openshift.org/latest/dev_guide/builds/build_inputs.html
-# oc login -u system:admin
+oc login -u system:admin
 
 # Create persisten volumes
-oc process -f  oc-persistentvolume-hostpath.yaml | oc create -f -
+# oc process -f  oc-persistentvolume-hostpath.yaml | oc create -f -
 
-# oc login -u developer
+oc login -u developer
 
 # Create new oc project
 oc new-project "${project}"
@@ -21,38 +21,38 @@ oc secrets new-sshauth ${secretname} --ssh-privatekey=$HOME/.ssh/id_rsa
 
 # Create builds for each docker image
 for c in "hdfs" "alluxio" "spark" "zeppelin"; do
-    oc process -v REPOSITORY=${repository} \
-                -v CONTEXTDIR="${c}" \
-                -v SECRETNAME="${secretname}" \
-                -v ID="${c}" \
+    oc process -p REPOSITORY=${repository} \
+                -p CONTEXTDIR="${c}" \
+                -p SECRETNAME="${secretname}" \
+                -p ID="${c}" \
                 -f oc-build-has.yaml | oc create -f -
 done
  
 # Deploy HDFS namenode 
 export hdfs_image=$(oc get is/hdfs --template="{{ .status.dockerImageRepository }}" --namespace ${project})
-oc process -v IMAGE=${hdfs_image} -v STORAGE="1Gi" -f "oc-deploy-hdfs-namenode.yaml" | oc create -f -
+oc process -p IMAGE=${hdfs_image} -p STORAGE="1Gi" -f "oc-deploy-hdfs-namenode.yaml" | oc create -f -
 
 
 # Deploy HDFS httpfs node 
-oc process -v IMAGE=${hdfs_image} -f "oc-deploy-hdfs-httpfs.yaml" | oc create -f -
+oc process -p IMAGE=${hdfs_image} -f "oc-deploy-hdfs-httpfs.yaml" | oc create -f -
 
 # Deploy Alluxio master
 export alluxio_image=$(oc get is/alluxio --template="{{ .status.dockerImageRepository }}" --namespace ${project})
-oc process -v IMAGE=${alluxio_image} -v STORAGE="1Gi" -f "oc-deploy-alluxio-master.yaml" | oc create -f -
+oc process -p IMAGE=${alluxio_image} -p STORAGE="1Gi" -f "oc-deploy-alluxio-master.yaml" | oc create -f -
 
 
 # Deploy Spark master
 export spark_image=$(oc get is/spark --template="{{ .status.dockerImageRepository }}" --namespace ${project})
-oc process -v IMAGE=${spark_image} -v STORAGE="1Gi" -f "oc-deploy-spark-master.yaml" | oc create -f -
+oc process -p IMAGE=${spark_image} -p STORAGE="1Gi" -f "oc-deploy-spark-master.yaml" | oc create -f -
 
 # Deploy three workers
-for id in $(seq 1 1 6); do
-    oc process -v ID=${id} -v IMAGE_SPARK="${spark_image}" -v IMAGE_ALLUXIO="${alluxio_image}" -v IMAGE_HDFS="${hdfs_image}" -v STORAGE="1Gi" -f "oc-deploy-has-node.yaml" | oc create -f -
+for id in $(seq 1 1 3); do
+    oc process -p ID=${id} -p IMAGE_SPARK="${spark_image}" -p IMAGE_ALLUXIO="${alluxio_image}" -p IMAGE_HDFS="${hdfs_image}" -p STORAGE="1Gi" -f "oc-deploy-has-node.yaml" | oc create -f -
 done
 
 # Deploy a Zeppelin client
 export zeppelin_image=$(oc get is/zeppelin --template="{{ .status.dockerImageRepository }}" --namespace ${project})
-oc process -v ID=0 -v IMAGE=${zeppelin_image} -v STORAGE="1Gi" -f "oc-deploy-zeppelin.yaml" | oc create -f -
+oc process -p ID=0 -p IMAGE=${zeppelin_image} -p STORAGE="1Gi" -f "oc-deploy-zeppelin.yaml" | oc create -f -
 
 # HDFS ports
 # MASTER 8020, 8022, 50070, 
