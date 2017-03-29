29874281c2b78affb330a022c7a4ff9b31ab96ed
Alvaro Alda <alvaro.alda@bbva.com>
2017-03-29 11:44:45 +0200

    Add rm option to HTTPFS client

--- data/httpfs.sh
+++ data/httpfs.sh
@@ -8,7 +8,7 @@ user="openshift"
 httpfs=${HTTPFS:-"http://hdfs-httpfs-dashboard-hasz.192.168.42.95.nip.io"}
 
 debug="-v -i"
-case $action in 
+case $action in
     upload)
         cat ${local_file}| curl $debug -X PUT \
             -T . \
@@ -18,16 +18,21 @@ case $action in
             -H "Content-Type: application/octet-stream" \
             -H "Transfer-Encoding: chunked" \
             -H "Cache-Control: no-cache" \
-            "${httpfs}/webhdfs/v1/${remote_filepath}?op=CREATE&user.name=${user}" 
+            "${httpfs}/webhdfs/v1/${remote_filepath}?op=CREATE&user.name=${user}"
     ;;
     ls)
         remote_filepath=${local_file}
         curl -L $debug -X GET \
-            "${httpfs}/webhdfs/v1/${remote_filepath}?op=LISTSTATUS&data=true&user.name=${user}" 
+            "${httpfs}/webhdfs/v1/${remote_filepath}?op=LISTSTATUS&data=true&user.name=${user}"
     ;;
     mkdir)
         remote_filepath=${local_file}
         curl -L $debug -X PUT \
-         "${httpfs}/webhdfs/v1/user/${user}/${remote_filepath}?op=MKDIRS&user.name=${user}" 
+         "${httpfs}/webhdfs/v1/user/${user}/${remote_filepath}?op=MKDIRS&user.name=${user}"
+    ;;
+    rm)
+        remote_filepath=${local_file}
+        curl -L $debug -X DELETE \
+        "${httpfs}/webhdfs/v1/user/${user}/${remote_filepath}?op=DELETE&user.name=${user}&recursive=true"
     ;;
 esac
