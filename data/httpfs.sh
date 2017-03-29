9b7e9fcdb7e3a13f7217bf5e0ac8e8264bc748c4
Gabriel Díaz López de la Llave <g.diaz.loepzllave@bbva.com>
2017-03-29 11:21:59 +0200

    refactor uploader to httpfs client

---
+++ data/httpfs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+action="$1"
+local_file="$2"
+remote_filepath="$3"
+
+user="openshift"
+httpfs=${HTTPFS:-"http://hdfs-httpfs-dashboard-hasz.192.168.42.95.nip.io"}
+"
+debug="-v -i"
+case $action in 
+    upload)
+        cat ${local_file}| curl $debug -X PUT \
+            -T . \
+            -H "Expect:" \
+            -H "accept-encoding: gzip, deflate" \
+            -H "Connection: keep-alive" \
+            -H "Content-Type: application/octet-stream" \
+            -H "Transfer-Encoding: chunked" \
+            -H "Cache-Control: no-cache" \
+            "${httpfs}/webhdfs/v1/${remote_filepath}?op=CREATE&user.name=${user}" 
+    ;;
+    ls)
+        remote_filepath=${local_file}
+        curl -L $debug -X GET \
+            "${httpfs}/webhdfs/v1/${remote_filepath}?op=LISTSTATUS&data=true&user.name=${user}" 
+    ;;
+    mkdir)
+        remote_filepath=${local_file}
+        curl -L $debug -X PUT \
+         "${httpfs}/webhdfs/v1/user/${user}/${remote_filepath}?op=MKDIRS&user.name=${user}" 
+    ;;
+esac
