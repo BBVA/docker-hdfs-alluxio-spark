2e38ef0a8f2207f4fbb0a9076b69522b5777ae1a
Alvaro Alda <alvaro.alda@bbva.com>
2017-04-17 17:38:37 +0200

    Add hdfs config files to alluxio installation

--- alluxio/boot.sh
+++ alluxio/boot.sh
@@ -35,6 +35,8 @@ master_node() {
 	case $action in
 		start)
 			export ALLUXIO_MASTER_HOSTNAME=${cluster_name}
+			config_hdfs "${HADOOP_CONF_DIR}/core-site.xml" ${CORE_SITE_CONF[@]}
+			config_hdfs "${HADOOP_CONF_DIR}/hdfs-site.xml" ${HDFS_SITE_CONF[@]}
 			if [ ! -f /opt/alluxio/conf/alluxio-env.sh ]; then
 				${ALLUXIO_PREFIX}/bin/alluxio bootstrapConf ${cluster_name}
 			fi
@@ -67,6 +69,8 @@ slave_node() {
 	case $action in
 		start)
 			export ALLUXIO_MASTER_HOSTNAME=${cluster_name}
+			config_hdfs "${HADOOP_CONF_DIR}/core-site.xml" ${CORE_SITE_CONF[@]}
+			config_hdfs "${HADOOP_CONF_DIR}/hdfs-site.xml" ${HDFS_SITE_CONF[@]}
 			${ALLUXIO_PREFIX}/bin/alluxio-start.sh worker NoMount
 		;;
 		stop)
@@ -82,13 +86,29 @@ slave_node() {
 	esac
 }
 
-config() {
+config_alluxio() {
 	local file="${1}"
 	shift
 	local conf=("${@}")
 	for p in "${conf[@]}"; do
 		echo ${p} >> ${file}
 	done
+}
+
+config_hdfs() {
+	local file="${1}"
+	shift
+	local conf=("${@}")
+	echo "<configuration>" > ${file}
+	for p in "${conf[@]}"; do
+		prop=$(echo ${p} | cut -f 1 -d '=')
+		val=$(echo ${p} | cut -f 2 -d '=')
+		echo "$file: $prop = $val"
+		echo "<property>" >> ${file}
+		echo "<name>${prop}</name><value>${val}</value>" >> ${file}
+		echo "</property>" >> ${file}
+	done
+	echo "</configuration>" >> ${file}
 }
 
 
@@ -128,6 +148,41 @@ default_properties=(
 	"alluxio.user.block.size.bytes.default=32MB"
 )
 
+# default config
+# For httpfs config
+# https://hadoop.apache.org/docs/current/hadoop-project-dist/hadoop-common/Superusers.html
+# insecure defaults
+core_site_default=(
+	"fs.defaultFS=hdfs://${cluster_name}:8020"
+	"io.file.buffer.size=131072"
+	"hadoop.proxyuser.openshift.hosts=172.16.0.0/12,10.0.0.0/8"
+	"hadoop.proxyuser.openshift.groups=openshift,root"
+	"hadoop.proxyuser.root.hosts=172.16.0.0/12,10.0.0.0/8"
+	"hadoop.proxyuser.root.groups=root"
+)
+
+# https://log.rowanto.com/why-datanode-is-denied-communication-with-namenode/
+# disable remote host name check, enable in production with a correct service
+# discovery reverse dns set up
+hdfs_site_default=(
+	"dfs.namenode.name.dir=file:///data/${cluster_name}/"
+	"dfs.blocksize=33554432"
+	"dfs.namenode.handler.count=100"
+	"dfs.namenode.servicerpc-address=hdfs://${cluster_name}:8022"
+	"dfs.namenode.datanode.registration.ip-hostname-check=false"
+	"dfs.datanode.data.dir=/data/${cluster_name}"
+	"dfs.client.use.datanode.hostname=true"
+	"dfs.datanode.use.datanode.hostname=true"
+)
+
+if [ "${CORE_SITE_CONF}z" == "z" ]; then
+	CORE_SITE_CONF=${core_site_default[@]}
+fi
+
+if [ "${HDFS_SITE_CONF}z" == "z" ]; then
+	HDFS_SITE_CONF=${hdfs_site_default[@]}
+fi
+
 trap "shut_down sigkill" SIGKILL
 trap "shut_down sigterm" SIGTERM
 trap "shut_down sighup" SIGHUP
@@ -137,7 +192,7 @@ trap "shut_down sigint" SIGINT
 
 setup_username
 
-config "${ALLUXIO_PREFIX}/conf/alluxio-site.properties" "${default_properties[@]}"
+config_alluxio "${ALLUXIO_PREFIX}/conf/alluxio-site.properties" "${default_properties[@]}"
 alluxio_handler ${node} ${action} ${cluster_name}
 
 sleep 2s
