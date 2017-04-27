341f21ef8f3bdd2d0003fb8f1c80cbc53a89bd64
Gabriel Díaz López de la Llave <gdiazlo@outlook.com>
2017-04-27 18:12:09 +0200

    refactor deploy config without env vars, using incorporated config

--- oc/oc-cluster.sh
+++ oc/oc-cluster.sh
@@ -33,12 +33,6 @@ done
 export hdfs_image=$(oc get is/hdfs --template="{{ .status.dockerImageRepository }}" --namespace ${project})
 oc process \
   -p IMAGE=${hdfs_image} \
-  -p CONF_FILES="${HADOOP_CONF_FILES}" \
-  -p CONF_VARS="${HADOOP_CONF_VARS}" \
-  -p CORE_SITE_CONF="${CORE_SITE_CONF}" \
-  -p HDFS_SITE_CONF="${HDFS_SITE_CONF}" \
-  -p HTTPFS_HTTP_PORT="${HTTPFS_HTTP_PORT}" \
-  -p HTTPFS_ADMIN_PORT="${HTTPFS_ADMIN_PORT}" \
   -f "oc-deploy-hdfs-namenode.yaml" | oc create -f -
 
 
@@ -46,56 +40,30 @@ oc process \
 # Deploy HDFS httpfs node
 oc process \
   -p IMAGE=${hdfs_image} \
-  -p CONF_FILES="${HADOOP_CONF_FILES}" \
-  -p CONF_VARS="${HADOOP_CONF_VARS}" \
-  -p CORE_SITE_CONF="${CORE_SITE_CONF}" \
-  -p HDFS_SITE_CONF="${HDFS_SITE_CONF}" \
-  -p HTTPFS_HTTP_PORT="${HTTPFS_HTTP_PORT}" \
-  -p HTTPFS_ADMIN_PORT="${HTTPFS_ADMIN_PORT}" \
   -f "oc-deploy-hdfs-httpfs.yaml" | oc create -f -
 
 # Deploy Alluxio master
 export alluxio_image=$(oc get is/alluxio --template="{{ .status.dockerImageRepository }}" --namespace ${project})
 oc process \
   -p IMAGE=${alluxio_image} \
-  -p CONF_FILES="${ALLUXIO_CONF_FILES}" \
-  -p CONF_VARS="${ALLUXIO_CONF_VARS}" \
-  -p CORE_SITE_CONF="${CORE_SITE_CONF}" \
-  -p HDFS_SITE_CONF="${HDFS_SITE_CONF}" \
-  -p ALLUXIO_CONF="${ALLUXIO_CONF}" \
   -p ALLUXIO_WORKER_MEMORY_SIZE="${ALLUXIO_WORKER_MEMORY_SIZE}" \
-  -p ALLUXIO_RAM_FOLDER="${ALLUXIO_RAM_FOLDER}" \
-  -p ALLUXIO_UNDERFS_ADDRESS="${ALLUXIO_UNDERFS_ADDRESS}" \
-  -p HADOOP_CONF_DIR="/opt/alluxio/conf" \
-	-f "oc-deploy-alluxio-master.yaml" | oc create -f -
+  -f "oc-deploy-alluxio-master.yaml" | oc create -f -
 
 
 # Deploy Spark master
 export spark_image=$(oc get is/spark --template="{{ .status.dockerImageRepository }}" --namespace ${project})
 oc process \
   -p IMAGE=${spark_image} \
-  -p CONF_FILES="${SPARK_CONF_FILES}" \
-  -p CONF_VARS="${SPARK_CONF_VARS}" \
-  -p CORE_SITE_CONF="${CORE_SITE_CONF}" \
-  -p HDFS_SITE_CONF="${HDFS_SITE_CONF}" \
-  -p SPARK_CONF="${SPARK_CONF}" \
-  -p HADOOP_CONF_DIR="/opt/spark/conf" \
   -p SPARK_MASTER_WEBUI_PORT="${SPARK_MASTER_WEBUI_PORT}" \
   -p SPARK_WORKER_MEMORY="${SPARK_WORKER_MEMORY}" \
   -p SPARK_WORKER_PORT="${SPARK_WORKER_PORT}" \
   -p SPARK_WORKER_WEBUI_PORT="${SPARK_WORKER_WEBUI_PORT}" \
   -p SPARK_DAEMON_MEMORY="${SPARK_DAEMON_MEMORY}" \
-	-f "oc-deploy-spark-master.yaml" | oc create -f -
+  -f "oc-deploy-spark-master.yaml" | oc create -f -
 
 # Deploy splark history server
 oc process \
   -p IMAGE=${spark_image} \
-  -p CONF_FILES="${SPARK_CONF_FILES}" \
-  -p CONF_VARS="${SPARK_CONF_VARS}" \
-  -p CORE_SITE_CONF="${CORE_SITE_CONF}" \
-  -p HDFS_SITE_CONF="${HDFS_SITE_CONF}" \
-  -p SPARK_CONF="${SPARK_CONF}" \
-  -p HADOOP_CONF_DIR="/opt/spark/conf" \
   -f "oc-deploy-spark-history.yaml" | oc create -f -
 
 
@@ -106,29 +74,13 @@ for id in $(seq 1 1 ${nodes}); do
       -p IMAGE_ALLUXIO="${alluxio_image}" \
       -p IMAGE_HDFS="${hdfs_image}" \
       -p "HDFS_MEMORY=1GB" \
-      -p HDFS_CONF_FILES="${HADOOP_CONF_FILES}" \
-      -p HDFS_CONF_VARS="${HADOOP_CONF_VARS}" \
-      -p CORE_SITE_CONF="${CORE_SITE_CONF}" \
-      -p HDFS_SITE_CONF="${HDFS_SITE_CONF}" \
-      -p HTTPFS_HTTP_PORT="${HTTPFS_HTTP_PORT}" \
-      -p HTTPFS_ADMIN_PORT="${HTTPFS_ADMIN_PORT}" \
-      -p ALLUXIO_CONF_FILES="${ALLUXIO_CONF_FILES}" \
-      -p ALLUXIO_CONF_VARS="${ALLUXIO_CONF_VARS}" \
-      -p ALLUXIO_CONF="${ALLUXIO_CONF}" \
       -p ALLUXIO_WORKER_MEMORY_SIZE="6GB" \
-      -p ALLUXIO_RAM_FOLDER="${ALLUXIO_RAM_FOLDER}" \
-      -p ALLUXIO_UNDERFS_ADDRESS="${ALLUXIO_UNDERFS_ADDRESS}" \
-      -p ALLUXIO_HADOOP_CONF_DIR="/opt/alluxio/conf" \
-      -p SPARK_HADOOP_CONF_DIR="/opt/spark/conf" \
-      -p SPARK_CONF_FILES="${SPARK_CONF_FILES}" \
-      -p SPARK_CONF_VARS="${SPARK_CONF_VARS}" \
-      -p SPARK_CONF="${SPARK_CONF}" \
       -p SPARK_MASTER_WEBUI_PORT="${SPARK_MASTER_WEBUI_PORT}" \
       -p SPARK_WORKER_MEMORY="6GB" \
       -p SPARK_WORKER_PORT="${SPARK_WORKER_PORT}" \
       -p SPARK_WORKER_WEBUI_PORT="${SPARK_WORKER_WEBUI_PORT}" \
       -p SPARK_DAEMON_MEMORY="${SPARK_DAEMON_MEMORY}" \
-    	-f "oc-deploy-has-node.yaml" | oc create -f -
+      -f "oc-deploy-has-node.yaml" | oc create -f -
 done
 
 # Deploy a Zeppelin client
