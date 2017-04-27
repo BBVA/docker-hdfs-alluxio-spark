3e83e143dab2b569fa506aa65b85d85a79c8c246
Gabriel Díaz López de la Llave <gdiazlo@outlook.com>
2017-04-27 18:28:37 +0200

    fix typos

--- oc/oc-cluster.sh
+++ oc/oc-cluster.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-source ../conf/hadoop.sh
-source ../conf/alluxio.sh
-source ../conf/spark.sh
-
-
-
 # basic project data
 export project="has"
 export repository="ssh://git@globaldevtools.bbva.com:7999/bglh/docker-hdfs-alluxio-spark.git"
@@ -22,7 +16,6 @@ oc secrets new-sshauth ${secretname} --ssh-privatekey=$HOME/.ssh/id_rsa
 # Create builds for each docker image
 for c in "hdfs" "alluxio" "spark" "spark-submitter" "zeppelin"; do
     oc process -p REPOSITORY=${repository} \
-
                 -p CONTEXTDIR="${c}" \
                 -p SECRETNAME="${secretname}" \
                 -p ID="${c}" \
@@ -35,8 +28,6 @@ oc process \
   -p IMAGE=${hdfs_image} \
   -f "oc-deploy-hdfs-namenode.yaml" | oc create -f -
 
-
-
 # Deploy HDFS httpfs node
 oc process \
   -p IMAGE=${hdfs_image} \
@@ -46,19 +37,18 @@ oc process \
 export alluxio_image=$(oc get is/alluxio --template="{{ .status.dockerImageRepository }}" --namespace ${project})
 oc process \
   -p IMAGE=${alluxio_image} \
-  -p ALLUXIO_WORKER_MEMORY_SIZE="${ALLUXIO_WORKER_MEMORY_SIZE}" \
+  -p ALLUXIO_WORKER_MEMORY_SIZE="6G" \
   -f "oc-deploy-alluxio-master.yaml" | oc create -f -
-
 
 # Deploy Spark master
 export spark_image=$(oc get is/spark --template="{{ .status.dockerImageRepository }}" --namespace ${project})
 oc process \
   -p IMAGE=${spark_image} \
-  -p SPARK_MASTER_WEBUI_PORT="${SPARK_MASTER_WEBUI_PORT}" \
-  -p SPARK_WORKER_MEMORY="${SPARK_WORKER_MEMORY}" \
-  -p SPARK_WORKER_PORT="${SPARK_WORKER_PORT}" \
-  -p SPARK_WORKER_WEBUI_PORT="${SPARK_WORKER_WEBUI_PORT}" \
-  -p SPARK_DAEMON_MEMORY="${SPARK_DAEMON_MEMORY}" \
+  -p SPARK_MASTER_WEBUI_PORT="8080" \
+  -p SPARK_WORKER_MEMORY="6G" \
+  -p SPARK_WORKER_PORT="35000" \
+  -p SPARK_WORKER_WEBUI_PORT="8081" \
+  -p SPARK_DAEMON_MEMORY="1G" \
   -f "oc-deploy-spark-master.yaml" | oc create -f -
 
 # Deploy splark history server
@@ -75,19 +65,19 @@ for id in $(seq 1 1 ${nodes}); do
       -p IMAGE_HDFS="${hdfs_image}" \
       -p "HDFS_MEMORY=1GB" \
       -p ALLUXIO_WORKER_MEMORY_SIZE="6GB" \
-      -p SPARK_MASTER_WEBUI_PORT="${SPARK_MASTER_WEBUI_PORT}" \
+      -p SPARK_MASTER_WEBUI_PORT="8080" \
       -p SPARK_WORKER_MEMORY="6GB" \
-      -p SPARK_WORKER_PORT="${SPARK_WORKER_PORT}" \
-      -p SPARK_WORKER_WEBUI_PORT="${SPARK_WORKER_WEBUI_PORT}" \
-      -p SPARK_DAEMON_MEMORY="${SPARK_DAEMON_MEMORY}" \
+      -p SPARK_WORKER_PORT="35000" \
+      -p SPARK_WORKER_WEBUI_PORT="8081" \
+      -p SPARK_DAEMON_MEMORY="1G" \
       -f "oc-deploy-has-node.yaml" | oc create -f -
 done
 
 # Deploy a Zeppelin client
-export zeppelin_image=$(oc get is/zeppelin --template="{{ .status.dockerImageRepository }}" --namespace ${project})
-oc process -p ID=0 \
-	-p IMAGE=${zeppelin_image} \
-	-f "oc-deploy-zeppelin.yaml" | oc create -f -
+# export zeppelin_image=$(oc get is/zeppelin --template="{{ .status.dockerImageRepository }}" --namespace ${project})
+# oc process -p ID=0 \
+# 	-p IMAGE=${zeppelin_image} \
+# 	-f "oc-deploy-zeppelin.yaml" | oc create -f -
 
 # HDFS ports
 # MASTER 8020, 8022, 50070,
