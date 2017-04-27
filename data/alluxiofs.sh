56a796dd99f1b59529d6d1e67843f5550bd01d4a
apenedo <icedfiend@gmail.com>
2017-04-27 11:28:49 +0200

    upload and download functionality for alluxiofs script

--- data/alluxiofs.sh
+++ data/alluxiofs.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-set -x
 action="$1"
 local_file="$2"
 remote_filepath="$3"
 
 alluxio_proxy=${ALLUXIO_PROXY:-"http://alluxio-master-rest-has.eurocloud.hyperscale.io"}
 
-debug="-v"
+debug="-s"
 case $action in
     upload)
-         curl $debug -L -X PUT \
-            -T ${local_file} \
-            -H "Expect:" \
-            -H "accept-encoding: gzip, deflate" \
-            -H "Connection: keep-alive" \
-            -H "Content-Type: application/octet-stream" \
-            -H "Transfer-Encoding: chunked" \
-            -H "Cache-Control: no-cache" \
-            "${alluxio_proxy}/webhdfs/v1/${remote_filepath}?op=CREATE&user.name=${user}"
+        stream_id=$(curl $debug -L -X POST \
+            "${alluxio_proxy}/api/v1/paths/${remote_filepath}/create-file")
+
+        re='^[0-9]+$'
+        if ! [[ ${stream_id} =~ $re ]] ; then
+           echo "error: ${stream_id}" >&2; exit 1
+        fi
+
+        curl $debug -L -H "Content-Type: application/octet-stream" -X POST \
+            -d @${local_file} \
+            "${alluxio_proxy}/api/v1/streams/${stream_id}/write"
+
+        curl $debug -L -X POST \
+            "${alluxio_proxy}/api/v1/streams/${stream_id}/close"
     ;;
     ls)
         remote_filepath=${local_file}
@@ -37,8 +41,19 @@ case $action in
     ;;
     get)
         remote_filepath=${local_file}
-        curl -L $debug -X GET \
-        "${alluxio_proxy}/webhdfs/v1/${remote_filepath}?op=OPEN&user.name=${user}"
+        stream_id=$(curl $debug -L -X POST \
+            "${alluxio_proxy}/api/v1/paths/${remote_filepath}/open-file")
+
+        re='^[0-9]+$'
+        if ! [[ ${stream_id} =~ $re ]] ; then
+           echo "error: ${stream_id}" >&2; exit 1
+        fi
+
+        curl $debug -L -X POST \
+            "${alluxio_proxy}/api/v1/streams/${stream_id}/read"
+
+        curl $debug -L -X POST \
+            "${alluxio_proxy}/api/v1/streams/${stream_id}/close"
     ;;
     *)
       echo "Invalid action: ${action}"
