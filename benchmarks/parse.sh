f3cf8a06ea8c8473ebbc88f8de2825814b012966
Gabriel Díaz López de la Llave <gdiazlo@outlook.com>
2017-05-17 13:21:36 +0200

    add log parser to get stats from jobs logs

---
+++ benchmarks/parse.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+parse() {
+	awk '/INFO TestDFSIO\$: $/ ,/\n/ {print $0}'  |  awk -v pod="$1" 'BEGIN{
+		i=0
+	}{ 
+	 split($0, kva, ": ")
+	 kv[i]=kva[2]
+	 i++
+	 }END {
+	 	printf("%s,",pod);
+	 	for  (k in kv) {
+	 		printf("%s,", kv[k]);
+	 	}
+	 	print "\n";
+	 }'
+}
+
+
+pods=$(oc get pods -l type=driver --template="{{ range .items }}{{.metadata.name }}{{end }}")
+
+for pod in $pods; do
+	oc logs $pod | parse $pod
+done
+
