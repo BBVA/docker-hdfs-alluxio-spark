8fb34f8eb421ba3ae8c93d2b3c1da54e2914b18e
Alvaro Alda <alvaro.alda@bbva.com>
2017-05-17 10:54:36 +0200

    Remove deprecated sparky project

--- sparky/src/main/scala/Conf.scala
+++
@@ -1,9 +0,0 @@
-import org.rogach.scallop.ScallopConf
-
-class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
-  val spark = opt[String](required = true)
-  val input = opt[String](required = true)
-  val output = opt[String](default = Some(input.toOption.get + "_out.txt"))
-  val samples = opt[Int](default= Some(10))
-  verify()
-}
