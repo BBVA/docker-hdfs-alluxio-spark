3c53950962fd24fbc568de6fe6de7d1f922e87b0
Gabriel Díaz López de la Llave <g.diaz.lopezllave@bbva.com>
2017-05-23 10:53:56 +0200

    tabs vs spaces

--- oc/oc-minishift.sh
+++ oc/oc-minishift.sh
@@ -16,11 +16,11 @@ oc secrets new-sshauth ${secretname} --ssh-privatekey=$HOME/.ssh/id_rsa
 
 # Create builds for each docker image
 for c in "hdfs" "alluxio" "spark" "spark-submitter" "zeppelin"; do
-    oc process -p REPOSITORY=${repository} \
-                -p CONTEXTDIR="${c}" \
-                -p SECRETNAME="${secretname}" \
-                -p ID="${c}" \
-                -f oc-build-has.yaml | oc create -f -
+	oc process -p REPOSITORY=${repository} \
+		-p CONTEXTDIR="${c}" \
+		-p SECRETNAME="${secretname}" \
+		-p ID="${c}" \
+		-f oc-build-has.yaml | oc create -f -
 done
 
 # Deploy HDFS namenode
@@ -38,40 +38,40 @@ export hdfs_image=$(oc get is/hdfs --template="{{ .status.dockerImageRepository 
 export alluxio_image=$(oc get is/alluxio --template="{{ .status.dockerImageRepository }}" --namespace ${project})
 ./remove_cluster_stuff.py "oc-deploy-alluxio-master.yaml" | oc process \
 	-p IMAGE=${alluxio_image} \
-  -p ALLUXIO_WORKER_MEMORY_SIZE="512MB" \
+	-p ALLUXIO_WORKER_MEMORY_SIZE="512MB" \
 	-f - | oc create -f -
 
 # Deploy Spark master
 export spark_image=$(oc get is/spark --template="{{ .status.dockerImageRepository }}" --namespace ${project})
 ./remove_cluster_stuff.py "oc-deploy-spark-master.yaml" | oc process \
 	-p IMAGE=${spark_image} \
-  -p SPARK_MASTER_WEBUI_PORT="8080" \
-  -p SPARK_WORKER_MEMORY="512M" \
-  -p SPARK_WORKER_PORT="35000" \
-  -p SPARK_WORKER_WEBUI_PORT="8081" \
-  -p SPARK_DAEMON_MEMORY="512M" \
+	-p SPARK_MASTER_WEBUI_PORT="8080" \
+	-p SPARK_WORKER_MEMORY="512M" \
+	-p SPARK_WORKER_PORT="35000" \
+	-p SPARK_WORKER_WEBUI_PORT="8081" \
+	-p SPARK_DAEMON_MEMORY="512M" \
 	-f - | oc create -f -
 
   # Deploy splark history server
 ./remove_cluster_stuff.py "oc-deploy-spark-history.yaml" | oc process \
-  -p IMAGE=${spark_image} \
-  -f - | oc create -f -
+	-p IMAGE=${spark_image} \
+	-f - | oc create -f -
 
 
 # Deploy three workers
 for id in $(seq 1 1 ${nodes}); do
-    ./remove_cluster_stuff.py "oc-deploy-has-node.yaml" | oc process -p ID=${id} \
-    	-p IMAGE_SPARK="${spark_image}" \
-    	-p IMAGE_ALLUXIO="${alluxio_image}" \
-    	-p IMAGE_HDFS="${hdfs_image}" \
-      -p "HDFS_MEMORY=512M" \
-      -p ALLUXIO_WORKER_MEMORY_SIZE="512MB" \
-      -p SPARK_MASTER_WEBUI_PORT="8080" \
-      -p SPARK_WORKER_MEMORY="512M" \
-      -p SPARK_WORKER_PORT="35000" \
-      -p SPARK_WORKER_WEBUI_PORT="8081" \
-      -p SPARK_DAEMON_MEMORY="512M" \
-    	-f - | oc create -f -
+	./remove_cluster_stuff.py "oc-deploy-has-node.yaml" | oc process -p ID=${id} \
+		-p IMAGE_SPARK="${spark_image}" \
+		-p IMAGE_ALLUXIO="${alluxio_image}" \
+		-p IMAGE_HDFS="${hdfs_image}" \
+		-p "HDFS_MEMORY=512M" \
+		-p ALLUXIO_WORKER_MEMORY_SIZE="512MB" \
+		-p SPARK_MASTER_WEBUI_PORT="8080" \
+		-p SPARK_WORKER_MEMORY="512M" \
+		-p SPARK_WORKER_PORT="35000" \
+		-p SPARK_WORKER_WEBUI_PORT="8081" \
+		-p SPARK_DAEMON_MEMORY="512M" \
+		-f - | oc create -f -
 done
 
 # Deploy a Zeppelin client
