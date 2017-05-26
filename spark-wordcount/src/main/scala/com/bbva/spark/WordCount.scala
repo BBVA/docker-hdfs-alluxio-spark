fbb210e37a40d4a9090e5268e050c074d6f0b86c
Alvaro Alda <alvaro.alda@bbva.com>
2017-05-26 11:00:09 +0200

    Simplify word count

--- spark-wordcount/src/main/scala/com/bbva/spark/WordCount.scala
+++ spark-wordcount/src/main/scala/com/bbva/spark/WordCount.scala
@@ -1,5 +1,6 @@
 package com.bbva.spark
 
+import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{SparkConf, SparkContext}
 
 object WordCount extends App {
@@ -12,13 +13,15 @@ object WordCount extends App {
 
     val sc = SparkContext.getOrCreate(sparkConf)
 
+    sc.hadoopConfiguration.set("fs.defaultFS", "hdfs://hdfs-namenode:8020")
+
     val lines = sc.textFile(conf.inputFile)
 
     val words = lines.flatMap(_.split(" "))
 
     val counts = words.map(word => (word, 1)).reduceByKey(_ + _)
 
-    println(s"Total: ${words.count()}")
+   // println(s"Total: ${words.count()}")
 
     println(s"Total distinct: ${counts.count()}")
 
