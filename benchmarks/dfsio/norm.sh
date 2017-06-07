ea195e4be6d3bd76e4ed6fe3917826782d8ef649
Gabriel Díaz López de la Llave <g.diaz.lopezllave@bbva.com>
2017-06-07 18:04:19 +0200

    update norm to get file size

--- benchmarks/dfsio/norm.sh
+++ benchmarks/dfsio/norm.sh
@@ -2,31 +2,31 @@
 
 cat $1 | awk 'BEGIN{
 	FS=","
-	printf("name,type,readcache,writecache,num_files, total_mb,mb/s,avg mb/s, std.dev.,time,zero\n")
+	printf("name,type,readcache,writecache,size,num_files, total_mb,mb/s,avg mb/s, std.dev.,time,zero\n")
 }
 /dfsio,read,cache,through/ {
-	printf("dfsio,read,cache,through,")
+	printf("dfsio,read,cache,through,%s,",$6)
 	for(i=9;i<=14;i++) {
 		printf("%s,",$i)
 	}
 	printf("0\n")
 }
 /dfsio,read,cache,cache,through/ {
-	printf("dfsio,read,cache,cache through,")
+	printf("dfsio,read,cache,cache through,%s,",$7)
 	for(i=10;i<=15;i++) {
 		printf("%s,",$i)
 	}
 	printf("0\n")
 }
 /dfsio,read,no,cache,through/ {
-	printf("dfsio,read,no cache,through,")
+	printf("dfsio,read,no cache,through,%s,",$7)
 	for(i=10;i<=15;i++) {
 		printf("%s,",$i)
 	}
 	printf("0\n")
 }
 /dfsio,read,no,cache,cache,through/ {
-	printf("dfsio,read,no cache,cache through,")
+	printf("dfsio,read,no cache,cache through,%s,",$8)
 	for(i=11;i<=16;i++) {
 		printf("%s,",$i)
 	}
@@ -34,28 +34,28 @@ cat $1 | awk 'BEGIN{
 }
 
 /dfsio,write,cache,through/ {
-	printf("dfsio,write,cache,through,")
+	printf("dfsio,write,cache,through,%s,",$6)
 	for(i=9;i<=14;i++) {
 		printf("%s,",$i)
 	}
 	printf("0\n")
 }
 /dfsio,write,cache,cache,through/ {
-	printf("dfsio,write,cache,cache through,")
+	printf("dfsio,write,cache,cache through,%s,",$7)
 	for(i=10;i<=15;i++) {
 		printf("%s,",$i)
 	}
 	printf("0\n")
 }
 /dfsio,write,no,cache,through/ {
-	printf("dfsio,write,no cache,through,")
+	printf("dfsio,write,no cache,through,%s,",$7)
 	for(i=10;i<=15;i++) {
 		printf("%s,",$i)
 	}
 	printf("0\n")
 }
 /dfsio,write,no,cache,cache,through/ {
-	printf("dfsio,write,no cache,cache through,")
+	printf("dfsio,write,no cache,cache through,%s,",$8)
 	for(i=11;i<=16;i++) {
 		printf("%s,",$i)
 	}
