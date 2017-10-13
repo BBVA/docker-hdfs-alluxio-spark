dec4b0ec73626f76aeb6b1d05d54ac55df19ea0b
Alvaro Alda <alvaro.alda@bbva.com>
2017-10-13 11:55:09 +0200

    Add license header to all relevant files

--- benchmarks/dfsio/norm.sh
+++ benchmarks/dfsio/norm.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright 2017 Banco Bilbao Vizcaya Argentaria S.A.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 cat $1 | sed 's/gb//g' | awk 'BEGIN{
 	FS=","
 	printf("name,type,readcache,writecache,size,num_files, total_mb,mb/s,avg mb/s, std.dev.,time,zero\n")
@@ -76,4 +90,3 @@ cat $1 | sed 's/gb//g' | awk 'BEGIN{
 	printf("0\n")
 }
 '
-
--- benchmarks/dfsio/parse.sh
+++ benchmarks/dfsio/parse.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-
+# Copyright 2017 Banco Bilbao Vizcaya Argentaria S.A.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # ----- TestDFSIO ----- : read
 # 12            Date & time: Mon May 22 10:19:51 UTC 2017
@@ -10,13 +22,13 @@
 # 16 Average IO rate mb/sec: 93.74904
 # 17 IO rate std deviation: 92.96005736672606
 # 18    Test exec time sec: 33.183
-    
-    
+
+
 parse() {
 	 awk -v pod="$1" 'BEGIN{
 		i=0
 	}
-	/INFO TestDFSIO\$: $/ ,/^\n/ { 
+	/INFO TestDFSIO\$: $/ ,/^\n/ {
 	 split($0, kva, ": ")
 	 kv[i]=kva[2]
 	 i++
@@ -36,4 +48,3 @@ for pod in $pods; do
 	podstr=$(echo $pod | sed 's/-/,/g')
 	oc logs $pod | parse $podstr
 done
-
